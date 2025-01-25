1e2a81e1fb881ff277f6ffe096ff94d3381b9d02
VIVPM <vivpm99@gmail.com>
2025-01-25 20:36:21 +0530

    newall2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { AllSerService } from './all-ser.service'; // Import the service
 
@@ -12,6 +11,7 @@ import { AllSerService } from './all-ser.service'; // Import the service
 export class AppComponent implements OnInit {
 
   title = "Clinic";
+  private inactivityTime: any;
 
 
   constructor(public allSerService: AllSerService) { }
@@ -19,9 +19,25 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     // Check authentication state on component initialization
     this.allSerService.checkAuthState();
+    this.resetTimer(); // Start the timer on initialization
+  }
+
+  // Function to reset the timer
+  resetTimer() {
+    clearTimeout(this.inactivityTime);
+    this.inactivityTime = setTimeout(() => {
+      this.logout(); // Call logout after 1 hour of inactivity
+    }, 3600000); // 1 hour in milliseconds
   }
 
   logout(): void {
     this.allSerService.logout();
+  }
+
+  // Listen for user activity
+  @HostListener('document:mousemove', ['$event'])
+  @HostListener('document:keypress', ['$event'])
+  onUserActivity(event: Event) {
+    this.resetTimer(); // Reset the timer on user activity
   }
 }
